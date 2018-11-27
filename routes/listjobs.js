d622a3740f414487d3d91685bd597962c256183c
Pankaj Jethwa <pankaj.jethwa@severntrent.co.uk>
2018-11-27 11:32:14 +0000

    further changes

--- routes/listjobs.js
+++ routes/listjobs.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a job list resource');
-  res.render('listJobs', { title: 'Express' });
+  res.render('listjobs', { title: 'Express' });
 });
 
 module.exports = router;
