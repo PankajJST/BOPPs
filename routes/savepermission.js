46b5d1b0a9b386d15c9556a557736eee0aee603d
Pankaj Jethwa <pankaj.jethwa@severntrent.co.uk>
2018-11-29 10:39:17 +0000

    more gorgeous bootstrap

--- routes/savepermission.js
+++ routes/savepermission.js
@@ -6,11 +6,12 @@ router.post('/', function(req, res) {
   console.log('Post a permission: ' + JSON.stringify(req.body));
   // res.send('respond with a job list resource');
   console.dir(req.body);
-  setInterval(() => {
-    console.log('waiting 3 secs...');
-  }, 3000);
+
   console.log("inserting name..." +req.body.custname1);
-  res.render('endpage', { title: 'Express' });
+
+  setTimeout(() => {
+     res.render('endpage', { title: 'Express' });
+  }, 3000);
 });
 
 /* POST users listing. */
