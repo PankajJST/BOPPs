d67050c8365c97de352871be9d9817a6b12c3b32
Pankaj Jethwa <pankaj.jethwa@severntrent.co.uk>
2018-12-04 14:00:59 +0000

    collapsable owner dets

--- routes/listjobs.js
+++ routes/listjobs.js
@@ -3,12 +3,15 @@ var router = express.Router();
 
 var pgp = require('pg-promise')(/*options*/)
 const db = pgp(process.env.DATABASE_URL || 'postgres://jgfhvhabskheos:3ea9f2ed6a86d0f0a58773b177a2f638efc773546a3ffefe2e7aadf6abe4d921@ec2-54-247-119-167.eu-west-1.compute.amazonaws.com:5432/d9ojqd5uc7s2v0?ssl=true');
-
+var sqlSelect = 'select jobno, name, address, tel, ownername, owneraddress, ownertel, date, actioned from custpermissions order by date desc';
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  db.manyOrNone('SELECT * from custpermissions order by date desc', 123)
+
+  
+  // db.manyOrNone('SELECT * from custpermissions order by date desc', 123)
+  db.manyOrNone(sqlSelect, 123)
   .then(function (data) {
     console.log('DATA:', data);
     console.dir(data);
